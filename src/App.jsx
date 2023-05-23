fc6c0af3107e172fcc653bcab7b562c7cad35904
elizbaryananush <elizbaryananush27@gmail.com>
2023-05-23 15:50:30 +0400

    personal portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,10 @@ import React, { useState } from 'react'
 import './App.scss'
 import Navbar from './Components/Navbar'
 import Layout from './components/Layout'
-import bLinks from 'C:/Users/User/Desktop/nushik/portfolio/src/Components/Links.jsx'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Projects from './Components/Projects'
 import About from './Components/About'
 
 function App() {
-
   return (
     <div className='app'>
       <Router>
