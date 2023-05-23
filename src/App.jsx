8c150c68c0db5980aaf1d512d54b3a122f41c965
elizbaryananush <elizbaryananush27@gmail.com>
2023-05-23 16:01:31 +0400

    jsdnnkj

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import Navbar from './Components/Navbar.jsx'
 import Layout from './components/Layout.jsx'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import About from './Components/About.jsx'
+import Header from './Components/Header'
+import Links from './Components/Links'
+import Projects from './Components/Projects'
+import Footer from './Components/Footer'
 
 function App() {
      
@@ -12,7 +16,12 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/' Component={Layout} />
+          <Route path='/' element={<div>
+            <Header/>
+            <Links />
+            <Projects/>
+            <Footer />
+          </div>} />
           <Route path='/aboutme' Component={About} />
         </Routes>
       </Router>
